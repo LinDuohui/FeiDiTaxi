3b696acf8128ef4df5170b06186e1b132f523bde
LinDuohui <13675183624@163.com>
2023-09-08 18:20:21 +0800

    服务间调用 redis缓存引入

--- api-passenger/src/main/java/com/linduohui/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/linduohui/apipassenger/service/VerificationCodeService.java
@@ -1,7 +1,14 @@
 package com.linduohui.apipassenger.service;
 
+import com.linduohui.apipassenger.remote.ServiceVerificationCodeClient;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.responese.NumberCodeResponse;
 import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Lin Duohui
@@ -9,11 +16,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VerificationCodeService {
+
+    @Autowired
+    private ServiceVerificationCodeClient serviceVerificationCodeClient;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    //乘客验证码的前缀
+    private String verificationCodePrefix = "passenger-verification-code-";
+
     public String generateCode(String phone){
         //调用验证码服务，获取验证码
         System.out.println("调用验证码服务，获取验证码");
+        ResponseResult<NumberCodeResponse> responseResponseResult = serviceVerificationCodeClient.getNumberCode(5);
+        int numberCode = responseResponseResult.getData().getNumberCode();
+        System.out.println("remoute numbercode:"+numberCode);
         //将验证码存入redis
         System.out.println("将验证码存入redis");
+        //key value 过期时间
+        String key = verificationCodePrefix+phone;
+        //存入redis
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
+
         //封装返回Json
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("code",200);
